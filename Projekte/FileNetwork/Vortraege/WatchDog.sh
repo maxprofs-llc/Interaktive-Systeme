9807955bbc0f7590fbb1fc198aa4bcdef652c50f
Anastasia Kazakova <reader.asja@gmail.com>
2012-07-11 23:36:40 +0200

    PDFs

--- Projekte/FileNetwork/Vortraege/WatchDog.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
- while true; do
-    if [ '1_BRK_Konzept_zur_Requirements_Analyse.mmd' -nt '1_BRK_Konzept_zur_Requirements_Analyse.lastseen' ]; then
-      cp 1_BRK_Konzept_zur_Requirements_Analyse.mmd 1_BRK_Konzept_zur_Requirements_Analyse.lastseen
-      make 
-    fi
-    sleep 1;
-done
